58f0c2b8c616107a4af6eb676a8e5061d075e7ae
Isabelli Lucena 11ATI <isabelli.llucena@gmail.com>
2025-10-21 20:23:21 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express, { response } from "express"
+
+const app = express()
+const port = 3333
+
+app.get("/", (request, response) => {
+    response.send("<h1>Olá, mundo! Sou o backend.</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}!`)
+})
